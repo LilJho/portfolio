09f57b10a0d1da2593160eeb362d5e168262cc8d
LilJho <jhonnelg25@gmail.com>
2022-10-06 00:06:02 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# portfolio
+ simple html css porfolio- still on going got stuck on what to do
---
+++ index.js
@@ -0,0 +1,6 @@
+// change navbar styles on scroll
+window.addEventListener("scroll", () => {
+  document
+    .querySelector("nav")
+    .classList.toggle("windows-scroll", window.scrollY > 0);
+});
